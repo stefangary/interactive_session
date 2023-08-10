c6c1bcf2acd766012489919b89c50ceb834a8402
Matthew Shaxted <mattshax@gmail.com>
2023-08-10 13:30:48 -0500

    Adding job dir creation for non-shared home directories

--- partition/session_wrapper.sh
+++ partition/session_wrapper.sh
@@ -45,6 +45,10 @@ $sshcmd cp "~/.ssh/id_rsa.pub" ${remote_session_dir}
 
 cat >> ${session_sh} <<HERE
 echo "Running in host \$(hostname)"
+
+# create the job directory in case homedir is not shared
+mkdir -p \${resource_jobdir}
+
 sshusercontainer="ssh -J ${resource_privateIp} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${USER_CONTAINER_HOST}"
 
 displayErrorMessage() {
